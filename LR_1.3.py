51837d12c158c2b49f4883076ee1ef387ae04af4
tm <91734001+pr0m31h3u5@users.noreply.github.com>
2023-02-12 15:25:39 +0500

    Add files via upload

---
+++ LR_1.3.py
@@ -0,0 +1,7 @@
+import math
+
+print ("sin(30) : ",  math.sin(30))
+print ("sin(0) : ",  math.sin(0))
+print ("sin(-2) : ",  math.sin(-2))
+print ("sin(math.pi) : ",  math.sin(math.pi))
+print ("sin(math.pi/3) : ",  math.sin(math.pi/3))
