c382bd855ade3030e7ed4ff529095a32e5b367d0
tm <91734001+pr0m31h3u5@users.noreply.github.com>
2023-02-13 19:57:37 +0500

    Add files via upload

--- LR2/LR_2.2.py
+++ LR2/LR_2.2.py
@@ -2,6 +2,6 @@ lenght = float(input("введите длину помещения: "))
 width = float(input("введите ширину помещения: "))
 hight = float(input("введите высоту помещения: "))
 s = lenght*width
-wall_s = lenght*hight
+wall_s = ((lenght*hight)*2)+((width*hight)*2)
 vol = lenght*width*hight
 print("площадь пола равна: ", s , "," , "площадь стен равна: ", wall_s, ",", "объём помещения равен: ", vol)
